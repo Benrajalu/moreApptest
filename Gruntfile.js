4591d219c929040240f5a7953519c506bc3f98dd
Benoit Rajalu <rajalubenoit@gmail.com>
2015-03-13 17:56:42 +0100

    Updating watched files

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,9 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/views/{,*/}*.html',
+          '<%= yeoman.app %>/views/templates/{,*/}*.html',
+          '<%= yeoman.app %>/scripts/{,*/}*.json',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
